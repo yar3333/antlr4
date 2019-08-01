3d61a233b816af620c65c6667f180dac0519babd
Yaroslav Sivakov <yar3333@gmail.com>
2019-08-01 11:24:08 +0300

    Minors+

--- runtime/Php/BufferedTokenStream.php
+++ runtime/Php/BufferedTokenStream.php
@@ -15,55 +15,21 @@ class BufferedTokenStream implements TokenStream
     /**
      * @var Token[]
      */
-    public $tokens;
+    public $tokens = [];
 
     /**
      * @var int
      */
-    public $index;
+    public $index = -1;
 
     /**
      * @var bool
      */
-    public $fetchedEOF;
+    public $fetchedEOF = false;
 
     function __construct(TokenSource $tokenSource)
     {
-        // The {@link TokenSource} from which tokens for this stream are fetched.
         $this->tokenSource = $tokenSource;
-
-        // A collection of all tokens fetched from the token source. The list is
-        // considered a complete view of the input once {@link //fetchedEOF} is set
-        // to {@code true}.
-        $this->tokens = [];
-
-        // The index into {@link //tokens} of the current token (next token to
-        // {@link //consume}). {@link //tokens}{@code [}{@link //p}{@code ]} should
-        // be
-        // {@link //LT LT(1)}.
-        //
-        // <p>This field is set to -1 when the stream is first constructed or when
-        // {@link //setTokenSource} is called, indicating that the first token has
-        // not yet been fetched from the token source. For additional information,
-        // see the documentation of {@link IntStream} for a description of
-        // Initializing Methods.</p>
-        $this->index = -1;
-
-        // Indicates whether the {@link Token//EOF} token has been fetched from
-        // {@link //tokenSource} and added to {@link //tokens}. This field improves
-        // performance for the following cases:
-        //
-        // <ul>
-        // <li>{@link //consume}: The lookahead check in {@link //consume} to
-        // prevent
-        // consuming the EOF symbol is optimized by checking the values of
-        // {@link //fetchedEOF} and {@link //p} instead of calling {@link
-        // //LA}.</li>
-        // <li>{@link //fetch}: The check to prevent adding multiple EOF symbols
-        // into
-        // {@link //tokens} is trivial with this field.</li>
-        // <ul>
-        $this->fetchedEOF = false;
     }
 
     function mark() : int
@@ -73,7 +39,7 @@ class BufferedTokenStream implements TokenStream
 
     function release(int $marker) : void
     {
-    // no resources to release
+        // no resources to release
     }
 
     function reset() : void
